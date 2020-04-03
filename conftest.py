0830d888bccdca61cf31f359b26010cb094ee946
ananas-ananas <anastarkgon@ya.ru>
2020-04-03 13:13:46 +0300

    settings added

--- conftest.py
+++ conftest.py
@@ -16,7 +16,6 @@ def browser(request):
     
     print("\nstart chrome browser for test..")
     browser = webdriver.Chrome(options=options)
-    browser.implicitly_wait(5)
         
     yield browser
     print("\nquit browser..")
